8778a6c3614f7b1697b3172157053b76c08c1534
Mohamedhabi <hm_habi@esi.dz>
2019-11-24 23:50:46 +0100

    Adding service 1 (Login user,adding and removing students)

--- Service_1/routes/api.php
+++ Service_1/routes/api.php
@@ -16,3 +16,20 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('me', 'AuthController@me');
+    Route::resource('student', 'API\StudentController')->except('update','create','edit');
+
+});
